6acee90d54e686581b285a096d06ac1653f85374
muna <mailstomuna@gmail.com>
2019-11-07 10:50:35 -0800

    MVC architecture getting template fragments

--- pet-web/src/main/java/com/animuna/pet/bootstrap/DataLoader.java
+++ pet-web/src/main/java/com/animuna/pet/bootstrap/DataLoader.java
@@ -23,13 +23,11 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstname("Mike");
         owner1.setLastname("Adenuga");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstname("Nita");
         owner2.setLastname("Scott");
         ownerService.save(owner2);
@@ -37,13 +35,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loading Owners...........");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstname("Sam");
         vet1.setLastname("Eze");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstname("Okonkwo");
         vet2.setLastname("Igbo");
         vetService.save(vet2);
