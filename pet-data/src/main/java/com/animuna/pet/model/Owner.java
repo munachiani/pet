a4355a8e98dfdefe980d5f726271f702b42c8c8d
muna <mailstomuna@gmail.com>
2019-11-08 10:12:52 -0800

    created Visit Entity

--- pet-data/src/main/java/com/animuna/pet/model/Owner.java
+++ pet-data/src/main/java/com/animuna/pet/model/Owner.java
@@ -1,5 +1,13 @@
 package com.animuna.pet.model;
 
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
 }
